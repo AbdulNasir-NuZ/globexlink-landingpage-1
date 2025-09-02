6b665c8b0c9a97fa907bd85eb443e8c14b6d6a98
AbdulNasir-NuZ <nasir.nuz@gmail.com>
2025-09-02 22:45:52 +0530

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Landing page 1',
+  description: 'landing page 1 ',
+  generator: 'Nasir',
 }
 
 export default function RootLayout({
